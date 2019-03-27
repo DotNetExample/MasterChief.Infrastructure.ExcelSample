ee44445008d6fa98eb6e92590e66df36e8e33acf
YanZhiwei <Yan.Zhiwei@outlook.com>
2019-03-27 23:44:32 +0800

    添加项目文件。

---
+++ MasterChief.DotNet.Infrastructure.ExcelSample/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MasterChief.DotNet.Infrastructure.ExcelSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnToExcel = new System.Windows.Forms.Button();
+            this.btnToDataTable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnToExcel
+            // 
+            this.btnToExcel.Location = new System.Drawing.Point(28, 23);
+            this.btnToExcel.Name = "btnToExcel";
+            this.btnToExcel.Size = new System.Drawing.Size(162, 23);
+            this.btnToExcel.TabIndex = 0;
+            this.btnToExcel.Text = "DataTable ToExcel";
+            this.btnToExcel.UseVisualStyleBackColor = true;
+            this.btnToExcel.Click += new System.EventHandler(this.BtnToExcel_Click);
+            // 
+            // btnToDataTable
+            // 
+            this.btnToDataTable.Location = new System.Drawing.Point(221, 23);
+            this.btnToDataTable.Name = "btnToDataTable";
+            this.btnToDataTable.Size = new System.Drawing.Size(162, 23);
+            this.btnToDataTable.TabIndex = 1;
+            this.btnToDataTable.Text = "Excel ToDataTable";
+            this.btnToDataTable.UseVisualStyleBackColor = true;
+            this.btnToDataTable.Click += new System.EventHandler(this.BtnToDataTable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 134);
+            this.Controls.Add(this.btnToDataTable);
+            this.Controls.Add(this.btnToExcel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel Sample";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToExcel;
+        private System.Windows.Forms.Button btnToDataTable;
+    }
+}
+
---
+++ MasterChief.DotNet.Infrastructure.ExcelSample/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Windows.Forms;
+using MasterChief.DotNet.Infrastructure.Excel;
+using MasterChief.DotNet.Infrastructure.Serializer;
+using MasterChief.DotNet4.Utilities.Common;
+
+namespace MasterChief.DotNet.Infrastructure.ExcelSample
+{
+    public partial class Form1 : Form
+    {
+        private readonly IExcelManger _excelManger = new NPOIManager();
+        private readonly ISerializer _jsonSerializer = new JsonSerializer();
+
+        private string _mockExcelPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnToExcel_Click(object sender, EventArgs e)
+        {
+            var mockTable = BuilderExcelData();
+            _mockExcelPath = $"D:\\ExcelSample{DateTime.Now.FormatDate(12)}.xls";
+            _excelManger.ToExcel(mockTable, "员工信息汇总", "员工列表", _mockExcelPath);
+            Process.Start(_mockExcelPath);
+        }
+
+        private DataTable BuilderExcelData()
+        {
+            var mockTable = new DataTable();
+            mockTable.Columns.Add(new DataColumn {ColumnName = "序号"});
+            mockTable.Columns.Add(new DataColumn {ColumnName = "姓名"});
+            mockTable.Columns.Add(new DataColumn {ColumnName = "工作单位"});
+            mockTable.Columns.Add(new DataColumn {ColumnName = "性别"});
+            mockTable.Columns.Add(new DataColumn {ColumnName = "入职时间"});
+
+            for (var i = 0; i < 100; i++)
+                mockTable.Rows.Add(i.ToString(), $"张{i}", $"李{i}计算机公司", i % 2 == 0 ? "男" : "女",
+                    DateTime.Now.AddDays(i));
+            return mockTable;
+        }
+
+        private void BtnToDataTable_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_mockExcelPath))
+            {
+                MessageBox.Show("请生成模拟测试EXCEL文件");
+                return;
+            }
+
+            var excleTable = _excelManger.ToDataTable(_mockExcelPath, 0, 1, 2);
+            var jsonText = _jsonSerializer.Serialize(excleTable);
+            MessageBox.Show(jsonText);
+        }
+    }
+}
